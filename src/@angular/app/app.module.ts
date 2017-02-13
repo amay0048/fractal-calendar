f9298c3a11c86c3d08e9ad8ead5a79a1cc24130b
Anthony May <anthonyjmay@gmail.com>
2017-02-13 23:54:44 +1100

    Example of utilising the component directly

--- src/@angular/app/app.module.ts
+++ src/@angular/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppComponent }  from './app.component';
 import { CalendarPicker }  from '../calendar-picker/calendar-picker.component';
 import { CalendarProvider }  from '../calendar-picker/calendar-picker.provider';
 
-import * as asb from 'asb-dnd';
+// import * as asb from 'asb-dnd';
+import * as asb2 from '../../../node_modules/asb-dnd/src/directives/asb-draggable_directive'
 
 @NgModule({
-  imports:      [ BrowserModule, asb.AsbDragAndDropModule ],
-  declarations: [ AppComponent, CalendarPicker ],
+  imports:      [ BrowserModule /* , asb.AsbDragAndDropModule */ ],
+  declarations: [ AppComponent, CalendarPicker, asb2.AsbDraggableDirective ],
   providers:    [ CalendarProvider ],
   bootstrap:    [ AppComponent ]
 })
